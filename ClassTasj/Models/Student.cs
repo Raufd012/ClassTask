88a7e7b5faab4d3185bedcd4644f120f43691e49
nazeani <tu7mp4w99@code.edu.az>
2024-04-08 13:46:47 +0400

    initial commit

---
+++ ClassTasj/Models/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTasj.Models
+{
+    internal class Student
+    {
+        private static int Count = 0;
+        public int Id { get; }
+        public string Fullname { get; }
+
+        public Student(string fullname)
+        {
+            if (!CheckFullname(fullname))
+
+                Console.WriteLine(!CheckFullname(fullname));
+
+            Fullname = fullname;
+            Count++;
+            Id = Count;
+        }
+
+        public static bool CheckFullname(string fullname)
+        {
+            if (string.IsNullOrWhiteSpace(fullname))
+                return false;
+
+            string[] parts = fullname.Split(' ');
+            if (parts.Length != 2)
+                return false;
+
+            foreach (string part in parts)
+            {
+                if (string.IsNullOrWhiteSpace(part) || !char.IsLetter(part[0]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}. {Fullname}";
+        }
+    }
+}
---
+++ ClassTasj/Program.cs
@@ -0,0 +1,42 @@
+﻿using ClassTasj.Models;
+
+namespace ClassTasj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Student student1 = new Student("Rauf Dostuyev");
+                Console.WriteLine(student1);
+
+                Student student2 = new Student("Ulvi Abdullazada");
+                Console.WriteLine(student2);
+
+                Student student3 = new Student("Sabir Guliyev");
+                Console.WriteLine(student3);
+
+                Student student4 = new Student("Nurlan Abbasov");
+                Console.WriteLine(student4);
+
+                Student student5 = new Student("Qerib Qeribzada");
+                Console.WriteLine(student5);
+
+                Student student6 = new Student("Kamil Khafarofh");
+                Console.WriteLine(student6);
+
+                Student student7 = new Student("Sahib Mammadov");
+                Console.WriteLine(student7);
+
+                Student student8 = new Student("Shahin Aliyev");
+                Console.WriteLine(student8);
+
+                Student student9 = new Student("Medet Quliyev");
+                Console.WriteLine(student9);
+            }
+
+        }
+    }
+    
+}
